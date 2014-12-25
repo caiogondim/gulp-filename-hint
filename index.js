f0f8858700e23a2ec41da99ff0027087432572f0
Caio Gondim <me@caiogondim.com>
2014-12-25 23:23:10 +0100

    Adds .jscsrc

--- index.js
+++ index.js
@@ -1,23 +1,22 @@
 /* jshint node: true */
 
-'use strict';
+'use strict'
 
-var gulpUtil = require('gulp-util');
-var through = require('through2');
+var gulpUtil = require('gulp-util')
+var through = require('through2')
 
 function getFilename(file) {
   return file.path.split(file.base)[1]
 }
 
 function validateOpts(opts, cb) {
-
   if (JSON.stringify(opts) === JSON.stringify({})) {
     cb(new gulpUtil.PluginError(
       'gulp-filename-hint',
       'options must be declared'
-    ));
+    ))
 
-    return false;
+    return false
   }
 
   // Checks if `opts.whiteList` is an Array
@@ -25,27 +24,27 @@ function validateOpts(opts, cb) {
     cb(new gulpUtil.PluginError(
       'gulp-filename-hint',
       'whiteList option must be an Array instance'
-    ));
+    ))
 
-    return false;
+    return false
   }
 
   // Checks if `opts.whiteList` is an Array of strings
   if (opts.whiteList !== undefined) {
-    var isWhiteListAnArrayOfStrings = true;
+    var isWhiteListAnArrayOfStrings = true
 
     opts.whiteList.forEach(function(item) {
       if (item.constructor !== String) {
         cb(new gulpUtil.PluginError(
           'gulp-filename-hint',
           'whiteList option must be an Array of String'
-        ));
-        isWhiteListAnArrayOfStrings = false;
+        ))
+        isWhiteListAnArrayOfStrings = false
       }
-    });
+    })
 
     if (!isWhiteListAnArrayOfStrings) {
-      return false;
+      return false
     }
   }
 
@@ -54,9 +53,9 @@ function validateOpts(opts, cb) {
     cb(new gulpUtil.PluginError(
       'gulp-filename-hint',
       'regExp option is required'
-    ));
+    ))
 
-    return false;
+    return false
   }
 
   // Checks if `opts.regExp` is a RegExp
@@ -64,43 +63,43 @@ function validateOpts(opts, cb) {
     cb(new gulpUtil.PluginError(
       'gulp-filename-hint',
       'regExp option must be a RegExp instance'
-    ));
+    ))
 
-    return false;
+    return false
   }
 
-  return true;
+  return true
 }
 
 function isInWhiteList(filename, whiteList) {
-  var index = whiteList.indexOf(filename);
-  return (index >= 0 && filename === whiteList[index]);
+  var index = whiteList.indexOf(filename)
+  return (index >= 0 && filename === whiteList[index])
 }
 
 module.exports = function(opts) {
-  opts = opts || {};
+  opts = opts || {}
 
-  var stream = through.obj(function (file, enc, cb) {
+  var stream = through.obj(function(file, enc, cb) {
     if (!validateOpts(opts, cb)) {
-      return;
+      return
     }
 
-    var filename = getFilename(file);
+    var filename = getFilename(file)
 
     if (opts.whiteList && isInWhiteList(filename, opts.whiteList)) {
-      cb(null, file);
-      return;
+      cb(null, file)
+      return
     }
 
     if (!opts.regExp.test(filename)) {
       this.emit('error', new gulpUtil.PluginError(
         'gulp-filename-hint',
         '“' + filename + '” is not a valid filename based on passed RegExp'
-      ));
+      ))
     }
 
-    cb(null, file);
-  });
+    cb(null, file)
+  })
 
-  return stream;
+  return stream
 }
